601e9783a1d5fa4af24033c1612a5ccb179f92b7
Не Еремей! <60701195+eremeyko@users.noreply.github.com>
2022-02-11 15:02:01 +0300

    Архивация по дням
    Нововведения:
    • Теперь чаты можно архивировать по четырём критериям: переписка не велась больше 3-х дней, больше 7, больше 30 и ввод своего числа дней
    Добавления: 
    • Обработка токена: если вы ввели неверный токен, то Вам напишет это и запросит его заново
    • Вывод информации об уже заархивированном чате (vk api не помечает заархивирован чат уже или нет)

--- archiveChats.py
+++ archiveChats.py
@@ -1,28 +1,49 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 from os import system
-from time import sleep
+from time import sleep, time
 
 from requests import post
 
 
-def getChats(token, i):
+def getDays(welcome_text):
+    while True:
+        system("cls")
+        check = int(
+            input(
+                f"{welcome_text}[?] Архивировать чаты с неактивностью более:\n ├[0] Все чаты | [1] 3 дня | [2] 7 дней | [3] 30 дней | [4] Свое значение\n └"
+            )
+        )
+        # Да, у меня старая версия питона, и что?
+        if check == 0:
+            return 0
+        elif check == 1:
+            return 3 * 86400
+        elif check == 2:
+            return 7 * 86400
+        elif check == 3:
+            return 30 * 86400
+        elif check == 4:
+            return int(input("[•] Введите количество дней: ")) * 86400
+        else:
+            continue
+
+
+def getChats(token, i, days):
     conversations_id = []
     chats = post(
         "https://api.vk.com/method/messages.getConversations",
         data={"offeset": i, "count": 200, "access_token": token, "v": 5.174},
     ).json()
-    for j in chats["response"]["items"]:
-        conversations_id.append(j["conversation"]["peer"]["id"])
+    if days == 0:
+        for j in chats["response"]["items"]:
+            conversations_id.append(j["conversation"]["peer"]["id"])
+    else:
+        for j in chats["response"]["items"]:
+            timeback = int(time()) - int(j["last_message"]["date"])
+            if timeback > days:
+                conversations_id.append(j["conversation"]["peer"]["id"])
 
-    return conversations_id
-
-
-def archiveChats(token, conversations_id):
-    successful_archive = 0
-    error_archive = 0
-    flood_control = 0
-    already_in_archive = 0
     conversations_id.remove(100)
     conversations_id.remove(
         post(
@@ -30,6 +51,15 @@ def archiveChats(token, conversations_id):
             data={"access_token": token, "v": "5.174"},
         ).json()["response"][0]["id"]
     )
+    return conversations_id
+
+
+def archiveChats(token, conversations_id, welcome_text):
+    successful_archive = 0
+    error_archive = 0
+    flood_control = 0
+    already_in_archive = 0
+
     for peer_id in conversations_id:
         response = post(
             "https://api.vk.com/method/messages.archiveConversation",
@@ -41,20 +71,20 @@ def archiveChats(token, conversations_id):
                 sleep(3)
                 system("cls")
                 print(
-                    f"[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive} \n └[f] Флуд контроль:\t{flood_control}"
+                    f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[v] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
                 )
                 conversations_id.insert(0, peer_id)
             if response["error"]["error_code"] == 100:
                 error_archive += 1
                 system("cls")
                 print(
-                    f"[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive} \n └[f] Флуд контроль:\t{flood_control}"
+                    f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[v] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
                 )
             elif response["error"]["error_code"] == 964:
                 already_in_archive += 1
                 system("cls")
                 print(
-                    f"[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive} \n └[f] Флуд контроль:\t{flood_control}"
+                    f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[v] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
                 )
                 sleep(10)
             else:
@@ -63,25 +93,40 @@ def archiveChats(token, conversations_id):
             successful_archive += 1
             system("cls")
             print(
-                f"[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive} \n └[f] Флуд контроль:\t{flood_control}"
+                f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[v] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
             )
-            pass
-    return 0
 
 
 if __name__ == "__main__":
-    print("Просто собирает все диалоги и отправляет их в архив")
-    token = input("Токен: ")
-    chats = post(
-        "https://api.vk.com/method/messages.getConversations",
-        data={"offeset": 0, "count": 200, "access_token": token, "v": 5.174},
-    ).json()["response"]["count"]
+    welcome_text = f"""Просто собирает все диалоги и отправляет их в архив
+
+Q: Зачем?\n\tA: Избавиться от мусора в чате\n  Q: А если мне нужны некоторые диалоги\n\tA: Тогда включите заранее на них уведомление и они сами вернутся в чат, когда поступит новое сообщение\n  Q: Почему он постоянно перезапускается?\n\tA: Особенность vk api, архивируется первые 200 чатов (Не включая Вас и id100 — их нельзя архивировать)
+{'_'*8}
+"""
+    while True:
+        system("cls")
+        token = input("Токен: ")
+
+        chats = post(
+            "https://api.vk.com/method/messages.getConversations",
+            data={"offeset": 0, "count": 200, "access_token": token, "v": 5.174},
+        ).json()
+
+        if "error" in chats:
+            print("Введён неверный токен")
+            sleep(2)
+            continue
+        else:
+            chats = chats["response"]["count"]
+            break
+
+    days = getDays(welcome_text)
 
     for j in range(0, chats, 200):
         system("cls")
         print(
-            f"\n  Q: Зачем?\n\tA: Избавиться от мусора в чате\n  Q: А если мне нужны некоторые диалоги\n\tA: Тогда включите заранее на них уведомление и они сами вернутся в чат, когда поступит новое сообщение\n  Q: Почему он постоянно перезапускается?\n\tA: Особенность vk api, архивируется первые 200 чатов (Не включая Вас и id100 — их нельзя архивировать)\n\nСобираем список\tАрхивированно чатов: ~{j+200 if chats > j else chats}..."
+            f"{welcome_text}Собираем список\nБудет заархивированно ~{j+200 if chats > j else chats} чатов..."
         )
-        list_id = getChats(token, j)
-        print(f"[i] Всего будет заархивировано {len(list_id)} чатов.")
-        check = archiveChats(token, list_id)
+        list_id = getChats(token, j, days)
+        if len(list_id) != 0:
+            archiveChats(token, list_id, welcome_text)

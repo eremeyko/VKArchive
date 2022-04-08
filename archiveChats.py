cbd0c3933b8aea12166cab7caa5ce138489973bb
Не Еремей! <60701195+eremeyko@users.noreply.github.com>
2022-04-08 16:24:04 +0300

    Новая версия
    * Нормальный счётчик уже заархивированных чатов
    * Избежание флуд контроля путём задержки и except'ов

--- archiveChats.py
+++ archiveChats.py
@@ -4,6 +4,10 @@ from os import system
 from time import sleep, time
 
 from requests import post
+from random import randint
+
+
+already_in_archive = 0
 
 
 def getDays(welcome_text):
@@ -31,34 +35,45 @@ def getDays(welcome_text):
 
 def getChats(token, i, days):
     conversations_id = []
-    chats = post(
-        "https://api.vk.com/method/messages.getConversations",
-        data={"offeset": i, "count": 200, "access_token": token, "v": 5.174},
-    ).json()
-    if days == 0:
-        for j in chats["response"]["items"]:
-            conversations_id.append(j["conversation"]["peer"]["id"])
-    else:
-        for j in chats["response"]["items"]:
-            timeback = int(time()) - int(j["last_message"]["date"])
-            if timeback > days:
-                conversations_id.append(j["conversation"]["peer"]["id"])
+    while True:
+        try:
+            chats = post(
+                "https://api.vk.com/method/messages.getConversations",
+                data={"offeset": i, "count": 200, "access_token": token, "v": 5.174},
+            ).json()
 
-    conversations_id.remove(100)
-    conversations_id.remove(
-        post(
-            "https://api.vk.com/method/users.get",
-            data={"access_token": token, "v": "5.174"},
-        ).json()["response"][0]["id"]
-    )
+            if days == 0:
+                for j in chats["response"]["items"]:
+                    conversations_id.append(j["conversation"]["peer"]["id"])
+            else:
+                for j in chats["response"]["items"]:
+                    timeback = int(time()) - int(j["last_message"]["date"])
+                    if timeback > days:
+                        conversations_id.append(j["conversation"]["peer"]["id"])
+            break
+        except KeyError:
+            sleep(randint(2,5))
+
+    sleep(randint(1,3))
+    try:
+        conversations_id.remove(
+            post(
+                "https://api.vk.com/method/users.get",
+                data={"access_token": token, "v": "5.174"},
+            ).json()["response"][0]["id"]
+        )
+        conversations_id.remove(100)
+    except ValueError:
+        pass 
+
     return conversations_id
 
 
 def archiveChats(token, conversations_id, welcome_text):
+    global already_in_archive
     successful_archive = 0
     error_archive = 0
     flood_control = 0
-    already_in_archive = 0
 
     for peer_id in conversations_id:
         response = post(
@@ -71,20 +86,20 @@ def archiveChats(token, conversations_id, welcome_text):
                 sleep(3)
                 system("cls")
                 print(
-                    f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[v] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
+                    f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[o] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
                 )
                 conversations_id.insert(0, peer_id)
             if response["error"]["error_code"] == 100:
                 error_archive += 1
                 system("cls")
                 print(
-                    f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[v] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
+                    f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[o] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
                 )
             elif response["error"]["error_code"] == 964:
                 already_in_archive += 1
                 system("cls")
                 print(
-                    f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[v] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
+                    f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[o] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
                 )
                 sleep(10)
             else:
@@ -93,9 +108,9 @@ def archiveChats(token, conversations_id, welcome_text):
             successful_archive += 1
             system("cls")
             print(
-                f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[v] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
+                f"{welcome_text}[•] Всего чатов: {len(conversations_id)}\n ├[v] Заархивировано:\t{successful_archive}\n ├[o] Уже в архиве:\t{already_in_archive}\n └[f] Флуд контроль:\t{flood_control}"
             )
-
+    already_in_archive += successful_archive
 
 if __name__ == "__main__":
     welcome_text = f"""Просто собирает все диалоги и отправляет их в архив
